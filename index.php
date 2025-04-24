2205cb6353b125c017092317ddd0e7168f6e2755
Nos <zvezdazanatih@gmail.com>
2025-04-24 12:44:28 +0700

    4

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<center>+++++++++++++PHP <b>and git</b> WORK!!!+++***</center>";
+echo "<center>++++++URA!+++++++PHP <b>and git</b> WORK!!!+++***</center>";
 
 
 $servername = "localhost";
